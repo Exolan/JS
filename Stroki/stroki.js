2d622d057aa3967d7e1d7e1de6ccaca662c91b5d
Evgeniy <evjekddd@mail.ru>
2022-02-01 14:58:48 +0300

    stroki

---
+++ Stroki/stroki.js
@@ -0,0 +1,20 @@
+let singleQuote = 'Строка в одиночных кавычках'
+let doubleQuote = "Строка в двойных кавычках"
+let tickQuote = `Строка в обратынх кавычках`
+
+let multilune1 = 'Строка\nс\nпереносом'
+let multilune2 = `Строка
+с
+переносами`
+
+let str1 = 'Первая строка'
+let str2 = 'Вторая строка'
+let contact = str1 +'\n' + str2
+let tickcontact = `${str1}
+${str2}`
+
+let greetings = 'Привет'
+let names = 'Тимофей'
+console.log(`${greetings}, ${names}! Добро пожаловать!`)
+
+
