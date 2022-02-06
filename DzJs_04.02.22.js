c2ac33aa384f44d42907fa6c66eaff762d7f7a88
Evgeniy <evjekddd@mail.ru>
2022-02-06 16:41:47 +0300

    06.02.22

--- DzJs_04.02.22.js
+++ DzJs_04.02.22.js
@@ -1,5 +1,5 @@
 let array=["evjekddd@mail.ru", "afasfdgg@mail.ru", "mopvmzpombpz@mail.ru", "gsgsgssc@mail.ru"]
-let arraychs=["afasfdgg@mail.ru", "mopvmzpombpz@mail.ru","evjekddd@mail.ru"]
+let arraychs=["afasfdgg@mail.ru"]
 function filter(array, arraychs){
   let arrayclear=[]
   for (let i = 0; i<array.length;i++){
