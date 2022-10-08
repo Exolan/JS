eed61c8012d8ca0b6fdd67519f08acf142426586
Exolan <97833724+Exolan@users.noreply.github.com>
2022-10-08 22:11:29 +0300

    Delete Laba3 directory

--- Laba3/zadaniye1.js
+++
@@ -1,9 +0,0 @@
-let password = "134_"
-if (password.length >= 4 && (password.includes("_") || password.includes("-")))
-  {
-    console.log("Пароль надежный")
-  }
-else
-  {
-    console.log("Пароль ненадежный")
-  }
